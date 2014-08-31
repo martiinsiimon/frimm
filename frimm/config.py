b457c396cf4b17cc5ad595c5356804e18c575c04
Martin Simon <martiin.siimon@gmail.com>
2014-08-31 18:29:10 +0200

    Configration of FRIMM

--- frimm/config.py
+++ frimm/config.py
@@ -1,15 +1,49 @@
-"""
-TODO:
-    restore()
-    store()
-    setATTR()
-    getATTR()
-    testRestore() -- tests
-    testStore() -- tests
-    testSetter() -- tests
-    testGetter() -- tests
-"""
+import json
+import os
+
 
 class Configuration(object):
-    def __init__(self):
-        pass
+    """
+    Configuration class managing  FRIMM's configuration, its storing and
+    restoring.
+    """
+
+    def __init__(self, config_dir=os.path.expanduser('~/.config/frimm/')):
+        self.configuration = {}
+        self.config_dir = config_dir
+        self.config_file = self.config_dir + 'config.json'
+        
+        if not os.path.isdir(self.config_dir):
+            os.mkdir(self.config_dir)
+
+    def __getitem__(self, key):
+        return self.configuration[key]
+
+    def __setitem__(self, key, value):
+        self.configuration[key] = value
+
+    def set_defaults(self):
+        self['print_results'] = 0
+        self['measure'] = 0
+        self['filter_path'] = os.path.expanduser('~/frimm/')
+        if not os.path.isdir(self['filter_path']):
+            os.mkdir(self['filter_path'])
+        self.store()
+
+    def restore(self):
+        if os.path.exists(self.config_file):
+            with open(self.config_file) as f:
+                file_content = f.read()
+            if not file_content:
+                self.set_defaults()
+            else:
+                file_json = json.loads(file_content)
+                for key, value in file_json.iteritems():
+                    self.configuration[key] = value
+        else:
+            self.set_defaults()
+
+    def store(self):
+        if len(self.configuration) > 0:
+            with open(self.config_file, 'w+') as f:
+                json.dump(self.configuration, f, indent=2)
---
+++ tests/config_tests.py
@@ -0,0 +1,55 @@
+#!/usr/bin/env python
+
+import unittest
+from frimm.config import Configuration
+import tempfile
+import os
+
+
+class TestConfiguration(unittest.TestCase):
+    def setUp(self):
+        self.config_dir = tempfile.mkdtemp()
+        self.config = Configuration(self.config_dir)
+
+    def tearDown(self):
+        try:
+            os.rmdir(self.config_dir)
+        except:
+            pass
+
+    def test_setter(self):
+        self.config['test1'] = 10
+        self.assertEqual(self.config.configuration['test1'], 10)
+
+    def test_getter(self):
+        self.config.configuration['test2'] = 20
+        self.assertEqual(self.config['test2'], 20)
+    
+    def test_store_restore(self):
+        self.config['print_results'] = 1
+        self.config['measure'] = 1
+        self.config.store()
+        self.assertEqual(self.config['print_results'], 1)
+        self.assertEqual(self.config['measure'], 1)
+        self.config['print_results'] = 0
+        self.config.restore()
+        self.assertEqual(self.config['print_results'], 1)
+        self.assertEqual(self.config['measure'], 1)
+
+    def test_set_defaults1(self):
+        self.config.set_defaults()
+        self.assertEqual(self.config['print_results'], 0)
+        self.assertEqual(self.config['measure'], 0)
+
+    def test_set_defaults2(self):
+        self.config.set_defaults()
+        self.config['print_results'] = 1
+        self.config.restore()
+        self.assertEqual(self.config['print_results'], 0)
+        self.assertEqual(self.config['measure'], 0)
+
+    def test_restore(self):
+        os.rmdir(self.config_dir)
+        self.config.restore()
+        self.assertEqual(self.config['print_results'], 0)
+        self.assertEqual(self.config['measure'], 0)
