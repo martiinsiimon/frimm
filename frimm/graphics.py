5956472af4ee8d455c46e70f1ceb432e11459911
Martin Simon <martiin.siimon@gmail.com>
2014-09-22 22:32:59 +0200

    Filtering works!
    Filtering is now working! Loading from ARGB and RGB24 PNG files is
    working and filtering via Python filters is working.

--- frimm/graphics.py
+++ frimm/graphics.py
@@ -1,16 +1,239 @@
 """
+Graphics module. Consists of object which represent graphic objects.
 """
+import cairo
 
-class Graphics(object):
+ARGB=0
+RGB24=1
+COLOR_MODEL = \
+    {0: {'name' : 'ARGB', 'pixel_width' : 4, 'a' : 3, 'r' : 2, 'g' : 1, 'b' : 0},\
+     1: {'name' : 'RGB24', 'pixel_width' : 4, 'a' : 3, 'r' : 2, 'g' : 1, 'b' : 0}}
+
+OUTPUT_COLOR_MODEL=RGB24
+
+class Pixel(object):
+    '''
+    '''
+    def __init__(self, r=0, g=0, b=0, a=0, v=0):
+        self.red = r
+        self.green = g
+        self.blue = b
+        self.alpha = v
+        self.value = v
+    
+    @property
+    def r(self):
+        return self.red
+
+    @r.setter
+    def r(self, value):
+        self.red = int(round(value))
+
+    @property
+    def g(self):
+        return self.green
+
+    @g.setter
+    def g(self, value):
+        self.green = int(round(value))
+
+    @property
+    def b(self):
+        return self.blue
+
+    @b.setter
+    def b(self, value):
+        self.blue = int(round(value))
+
+    @property
+    def a(self):
+        return self.alpha
+
+    @a.setter
+    def a(self, value):
+        self.alpha = int(round(value))
+
+    @property
+    def v(self):
+        return self.value
+        
+    @v.setter
+    def v(self, value):
+        self.value = int(round(value))
+        self.r = int(round(value))
+        self.g = int(round(value))
+        self.b = int(round(value))
+        self.a = int(round(value))
+        
+
+class Frame(object):
+    '''
+    '''
+    def __init__(self, data=None):
+        if data:
+            self.width = data.get_width()
+            self.height = data.get_height()
+            self.data = self._from_cairo_to_internal(data)
+        else:
+            self.data = []
+            self.width = 0
+            self.height = 0
+            
+        self.dirty = False
+        
+
+    def __getitem__(self, index):
+        return self.data[index]
+    
+    def __setitem__(self, index, value):
+        self.data.insert(index, value)
+        self.dirty = True
+
+    def _from_cairo_to_internal(self, input_data):
+        '''
+        Convert given cairo data to internal data
+        '''
+        data = input_data.get_data()
+        color_format = input_data.get_format()
+        pixel_width = COLOR_MODEL[color_format]['pixel_width']
+        output = []
+
+        total_width = self.width * pixel_width
+        
+        if color_format  == ARGB:
+            for y in range(0, self.height):
+                for x in range(0, total_width, pixel_width):
+                    pix = Pixel()
+                    pix.r = ord(data[y * total_width + x + COLOR_MODEL[color_format]['r']])
+                    pix.g = ord(data[y * total_width + x + COLOR_MODEL[color_format]['g']])
+                    pix.b = ord(data[y * total_width + x + COLOR_MODEL[color_format]['b']])
+                    pix.a = ord(data[y * total_width + x + COLOR_MODEL[color_format]['a']])
+                    output.insert(y * self.width + x/pixel_width, pix)
+        elif color_format == RGB24:
+            for y in range(0, self.height):
+                for x in range(0, total_width, pixel_width):
+                    pix = Pixel()
+                    pix.r = ord(data[y * total_width + x + COLOR_MODEL[color_format]['r']])
+                    pix.g = ord(data[y * total_width + x + COLOR_MODEL[color_format]['g']])
+                    pix.b = ord(data[y * total_width + x + COLOR_MODEL[color_format]['b']])
+                    output.insert(y * self.width + x/pixel_width, pix)
+        else:
+            assert RuntimeError, 'No other color format is supported!'
+        
+        return output
+
+    def update_cairo(self, data_pointer, format_pointer):
+        '''
+        Update cairo data from internal buffer to the given pointer
+        '''
+        pixel_width = COLOR_MODEL[OUTPUT_COLOR_MODEL]['pixel_width']
+        
+        for y in range(0, self.height):
+            for x in range(0, self.width):
+                pix = self[y * self.width + x]
+
+                data_pointer[y * self.width * pixel_width + x * pixel_width + COLOR_MODEL[OUTPUT_COLOR_MODEL]['r']] = chr(pix.r)
+                data_pointer[y * self.width * pixel_width + x * pixel_width + COLOR_MODEL[OUTPUT_COLOR_MODEL]['g']] = chr(pix.g)
+                data_pointer[y * self.width * pixel_width + x * pixel_width + COLOR_MODEL[OUTPUT_COLOR_MODEL]['b']] = chr(pix.b)
+                data_pointer[y * self.width * pixel_width + x * pixel_width + COLOR_MODEL[OUTPUT_COLOR_MODEL]['a']] = chr(255) #  unused
+        
+        self.dirty = False
+
+        format_pointer = OUTPUT_COLOR_MODEL
+
+    def get_cairo_data(self, color_format):
+        '''
+        Return converted data to cairo format
+        '''
+        return self._from_internal_to_cairo(self.data, color_format)
+
+
+class Image(object):
     """
-    Simple graphics object. Contains data of image frame including metadata, handlig methods and some simple operations. This class is designed to be parent of other classes.
+    Image object. This class is meant to perform store and load file
+    operations of image files.
     TODO:
-        setData()
-        getData()
-        setColorModel()
-        getColorModel()
         convertColorModel()
         dumpImageInformation()
+    """
+
+    def __init__(self, filename, filetype):
+        self.filetype = filetype
+        self.filename = filename
+        self.cairo_image_surface = None
+        self.data = None
+        self.width = 0
+        self.height = 0
+        self.load()
+
+    def load(self):
+        """
+        Load image from given. Currently, only PNG files are supported
+        """
+        if self.filetype == 'png':
+            self.cairo_image_surface = \
+                cairo.ImageSurface.create_from_png(self.filename)
+            self.data = Frame(data=self.cairo_image_surface)
+            self.width = self.data.width
+            self.height = self.data.height
+        else:
+            raise TypeError('Not supported file type!')
+
+    def store(self, new_filename=None):
+        """
+        Store this object as image of type PNG. If no new filename is
+        provided, old filename extended with '_new' wil be used
+        """
+        if self.cairo_image_surface:
+            if not new_filename:
+                new_filename = self.filename + '_new'
+
+            if self.filetype == 'png':
+                self.cairo_image_surface.write_to_png(new_filename)
+            else:
+                raise TypeError('Not supported file type!')
+
+    @property
+    def cairo_data(self):
+        if self.data.dirty:
+            self.data.update_cairo(self.cairo_image_surface.get_data(), self.cairo_image_surface.get_format())
+        return self.cairo_image_surface
+    
+    @cairo_data.setter
+    def cairo_data(self, value):
+        self.cairo_image_surface = value
+
+
+class Camera(object):
+    """
+    Camera object. This object provides operation needed to get data from
+    built-in or any other attached real-time camera.
+    TODO:
+        convertColorModel()
+        dumpImageInformation()
+        open()
+        getFrame()
+        getCameraInformation() -- or more methods
+    """
+
+    def __init__(self):
+        pass
+
+
+class Video(object):
+    """
+    Video object. This class is similar to Image object, but handles video
+    files. Rather than store and load images provides functions to load
+    and store videos, get next frame and so on.
+    TODO:
+        convertColorModel()
+        dumpImageInformation()
+        load()
+        store()
+        modifyFrame()
+        getFrame()
+        getNextFrame()
+        getVideoInformation() -- or more methods
     """
 
     def __init__(self):
--- frimm/window.py
+++ frimm/window.py
@@ -1,20 +1,32 @@
-#-*- coding: UTF-8 -*-
+# -*- coding: UTF-8 -*-
 """
-TODO:
-    finish signal handlers
 """
 import os
 import sys
+import cairo
 from gi.repository import Gtk
 
+from graphics import Image
+from config import Configuration
+from filters import Filters
+import time
+
 class MainWindow(object):
+    """
+    TODO:
+        finish signal handlers
+         fix todos
+    """
+
     def __init__(self):
         if os.path.exists('../data/window.ui'):
             self.gladefile = '../data/window.ui'
+        elif os.path.exists('data/window.ui'):
+            self.gladefile = 'data/window.ui'
         else:
             path = os.path.abspath(
                 os.path.join(__file__, os.path.pardir, os.path.pardir))
-            if path is '/': # in case the path is /bin/frimm
+            if path is '/':  # in case the path is /bin/frimm
                 path = '/usr'
             self.gladefile = path + '/share/frimm/ui/window.ui'
 
@@ -26,40 +38,196 @@ class MainWindow(object):
 
         if os.path.exists('../data/icons/frimm.png'):
             self.window.set_icon_from_file('../data/icons/frimm.png')
+        elif os.path.exists('data/icons/frimm.png'):
+            self.window.set_icon_from_file('data/icons/frimm.png')
         else:
             path = os.path.abspath(
                 os.path.join(__file__, os.path.pardir, os.path.pardir))
             if path is '/':
                 path = '/usr'
-            self.window.set_icon_from_file(os.path.join(path,
-                'share/icons/hicolor/48x48/apps/frimm.png'))
+            self.window.set_icon_from_file(
+                os.path.join(path,
+                             'share/icons/hicolor/48x48/apps/frimm.png'))
+
+        self.original_image = None
+        self.result_image = None
+
+        self.config = Configuration()
+        self.config.restore()
+        self.filters = Filters(self.config)
+        
+        self.filter_author_label = self.builder.get_object('filter_author_label')
+        self.filter_description_textview = self.builder.get_object('filter_description_textview')
+        self.filter_details_box = self.builder.get_object('filter_details_box')
+        self.statusbar = self.builder.get_object('statusbar')
+        self.statusbar_context_id = self.statusbar.get_context_id('statusbar_note')
+        
+        self.update_filters_combobox()
 
         self.window.show_all()
 
-    def on_main_window_destroy(self, object, data=None):
-        print("DBG: on_main_window_destroy")
+    def on_main_window_destroy(self, window):
+        print('DBG: on_main_window_destroy')
         sys.exit()
-    
-    def on_result_area_draw(self, object, data=None):
+
+    def on_comboboxfilter_changed(self, combo):
+        print("DBG: on_comboboxfilter_changed")
+        tree_iter = combo.get_active_iter()
+        if tree_iter != None:
+            model = combo.get_model()
+            selected_name, selected_key = model[tree_iter]
+            if selected_key == 'no_filter':
+                self.filters.active = None
+                self.filter_details_box.set_visible(False)
+            else:
+                self.filters.active = self.filters[selected_name]
+                self.filter_author_label.set_text(self.filters.active.author_name)
+                textbuffer = Gtk.TextBuffer()
+                textbuffer.set_text(self.filters.active.description)
+                self.filter_description_textview.set_buffer(textbuffer)
+                self.filter_details_box.set_visible(True)
+
+    def on_result_area_draw(self, screen, cairo_context):
+        if not self.result_image:
+            return
+
+        win_w = screen.get_allocated_width()
+        win_h = screen.get_allocated_height()
+
+        img_w = self.result_image.width
+        img_h = self.result_image.height
+
+        win_ratio = win_w / float(win_h)
+        img_ratio = img_w / float(img_h)
+
+        if img_ratio > win_ratio:
+            scale = win_w / float(img_w)
+        else:
+            scale = win_h / float(img_h)
+
+        cairo_context.translate((win_w / 2.0) - ((img_w * scale) / 2.0), (win_h / 2.0) - ((img_h * scale) / 2.0))
+        cairo_context.scale(scale, scale)
+
+        cairo_context.set_source_surface(self.result_image.cairo_data, 0, 0)
+        cairo_context.paint()
+
         print("DBG: on_result_area_draw")
-    
-    def on_original_area_button_press_event(self, object, data=None):
+
+    def on_original_area_press_event(self, drawing_area, button):
+        if not self.original_image:
+            return
+
         print("DBG: on_original_area_button_press_event")
-    
-    def on_original_area_draw(self, object, data=None):
+
+    def on_original_area_draw(self, screen, cairo_context):
+        if not self.original_image:
+            return
+
+        win_w = screen.get_allocated_width()
+        win_h = screen.get_allocated_height()
+
+        img_w = self.original_image.width
+        img_h = self.original_image.height
+
+        win_ratio = win_w / float(win_h)
+        img_ratio = img_w / float(img_h)
+
+        if img_ratio > win_ratio:
+            scale = win_w / float(img_w)
+        else:
+            scale = win_h / float(img_h)
+
+        cairo_context.translate((win_w / 2.0) - ((img_w * scale) / 2.0), (win_h / 2.0) - ((img_h * scale) / 2.0))
+        cairo_context.scale(scale, scale)
+
+        cairo_context.set_source_surface(self.original_image.cairo_data, 0, 0)
+        cairo_context.paint()
+
         print("DBG: on_original_area_draw")
-    
-    def on_quit_button_clicked(self, object, data=None):
+
+    def on_quit_button_clicked(self, button):
         print("DBG: on_quit_button_clicked")
         sys.exit()
-    
-    def on_proceed_button_clicked(self, object, data=None):
+
+    def on_proceed_button_clicked(self, button):
+        if not self.original_image:
+            self.update_statusbar('You need to load a source before filter execution!')
+            return
+        
+        if not self.filters.active:
+            self.update_statusbar('You need to set filter first!')
+            return
+        
+        self.result_image = Image(filename=self.original_image.filename, filetype='png')
+        
+        self.result_image.cairo_image_surface.flush()
+        
+        total_time = self.filters.execute_active(self.original_image.data, self.result_image.data)
+        self.result_image.data.dirty = True
+        
+        self.result_image.cairo_image_surface.mark_dirty()
+        win = self.builder.get_object('result_area').get_window()
+        win.invalidate_rect(None, False)
+
+        msg = 'Filter \'%s\' executed.' % self.filters.active.name
+        if self.config['measure']:
+            msg += ' Total time: %f' % total_time 
+        self.update_statusbar(msg)
+
         print("DBG: on_proceed_button_clicked")
-    
-    def on_export_button_clicked(self, object, data=None):
+        
+
+    def on_export_button_clicked(self, button):
+        if not self.result_image:
+            self.update_statusbar('You need to execute filtering before exporting results!')
+            return
+            
+        # TODO add filechooser dialog
+        self.result_image.store()
         print("DBG: on_export_button_clicked")
-    
-    def on_load_button_clicked(self, object, data=None):
+
+    def on_load_button_clicked(self, button):
+        dialog =\
+            Gtk.FileChooserDialog('Open file', self.window,\
+            Gtk.FileChooserAction.OPEN,\
+            (Gtk.STOCK_CANCEL, Gtk.ResponseType.CANCEL,\
+            Gtk.STOCK_OPEN, Gtk.ResponseType.OK))
+
+        filter_png = Gtk.FileFilter()
+        filter_png.set_name('PNG Image')
+        filter_png.add_mime_type('image/png')
+        dialog.add_filter(filter_png)
+
+        res = dialog.run()
+        if res == Gtk.ResponseType.OK:
+            if dialog.get_filter().get_name() == 'PNG Image':
+                start_time = time.time()
+                self.original_image =\
+                    Image(filename=dialog.get_filename(), filetype='png')
+                end_time = time.time()
+                self.update_statusbar("Total time to load: %f" % (end_time - start_time))
+                # TODO add statusbar info
+            else:
+                pass
+
+        dialog.destroy()
         print("DBG: on_load_button_clicked")
-    
-    
+
+    def update_statusbar(self, text):
+        self.statusbar.remove_all(self.statusbar_context_id)
+        self.statusbar.push(self.statusbar_context_id, text)
+
+    def update_filters_combobox(self):
+        self.filters.refresh()
+        self.filters_liststore = Gtk.ListStore(str, str)
+        if len(self.filters) > 0:
+            self.filters_liststore.append(['Select filter', 'no_filter'])
+            for filtname in self.filters:
+                self.filters_liststore.append([filtname, filtname])
+        else:
+            self.filters_liststore.append(['No filter found!', 'no_filter'])
+        self.comboboxfilter = self.builder.get_object('comboboxfilter')
+        self.comboboxfilter.set_model(self.filters_liststore)
+        self.comboboxfilter.set_active(0)
+        
+        print("DBG: update_filters_combobox")
