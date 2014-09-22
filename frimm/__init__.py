4adab99e457b8680fe875a679878cf649077bf84
Martin Simon <martiin.siimon@gmail.com>
2014-09-22 22:48:25 +0200

    Updated package relevant file

--- frimm/__init__.py
+++ frimm/__init__.py
@@ -1,7 +1,6 @@
-# -*- coding: UTF-8 -*-
-__all__ = ['window', 'io', 'graphics', 'filter', 'config']
+"""
+"""
+__all__ = ['window', 'graphics', 'filters', 'config']
 __author__ = 'Martin Simon <martiin.siimon@gmail.com>'
 __license__ = 'MIT'
-__name__ = 'frimm'
 __version__ = '0.1-alpha1'
-__package__ = 'frimm'
