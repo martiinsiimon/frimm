6050c18fc9f5a60e734466ef0bb430b61c39e8b1
Martin Simon <martiin.siimon@gmail.com>
2014-09-23 19:08:07 +0200

    Fixed and extended filters.
    Grayscale filter has been fixed according to new pixel accessing and
    storing. Also four more filters was been added, namely red, green and
    blue filters and simple inversion.

--- data/filters/inverse.py
+++ data/filters/inverse.py
@@ -1,13 +1,14 @@
 __author_name__ = 'Martin Simon'
 __author_mail__ = 'martiin.siimon@gmail.com'
-__filter_name__ = 'Grayscale'
-__filter_description__ = 'Simple grayscale filter with no options. Only converts color image to scale of gray.'
+__filter_name__ = 'Inverse'
+__filter_description__ = 'Simple inverse filter with no options. All color channels are reverted.'
 
 
 def process(in_img, out_img):
     width = in_img.width
     height = in_img.height
     pix_w = in_img.pixel_width
+    
     r_in_off = in_img.r_offset
     g_in_off = in_img.g_offset
     b_in_off = in_img.b_offset
@@ -19,10 +20,6 @@ def process(in_img, out_img):
     for y in range(height):
         for x in range(width):
             pix_index = y * width * pix_w + x * pix_w 
-            res = \
-                in_img[pix_index + r_in_off] * 0.299 + \
-                in_img[pix_index + g_in_off] * 0.587 + \
-                in_img[pix_index + b_in_off] * 0.114
-            out_img[pix_index + r_out_off] = \
-                out_img[pix_index + g_out_off] = \
-                out_img[pix_index + b_out_off] = res
+            out_img[pix_index + r_out_off] = 255 - in_img[pix_index + r_in_off]
+            out_img[pix_index + g_out_off] = 255 - in_img[pix_index + g_in_off]
+            out_img[pix_index + b_out_off] = 255 - in_img[pix_index + b_in_off]
